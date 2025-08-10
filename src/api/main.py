6ca996782417e142195e7cc3798be5eb3298ee37
Karen Scheid <kscheid@givelify.com>
2025-08-10 15:12:29 -0500

    .

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
-from prometheus_fastap
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 # Initialize FastAPI app with metadata
 app = FastAPI(
@@ -33,7 +34,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-Instrmentator().instrument(app).expose(app)
+Instrumentator().instrument(app).expose(app)
 
 # Health check endpoint
 @app.get("/health", response_model=dict)
